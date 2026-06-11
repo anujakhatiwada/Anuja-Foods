299d55d53f4bf9c34f0f5c0da4b9575c755b1821
anujakhatiwada <anujakhatiwada512@gmail.com>
2026-06-11 11:04:34 +0545

    final

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(cookieParser())
 app.use(cors({
-  origin:"http://localhost:5173", 
+  origin:"https://anuja-foods-frontend.vercel.app", 
   credentials:true
 }))
 app.use(express.json());
